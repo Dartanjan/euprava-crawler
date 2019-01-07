f129737773dab92ec8807656c439c28c04e62df2
Darko Stanimirovic <darko.stanimirovic@gmail.com>
2019-01-07 17:15:01 +0100

    Schedule crawl command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-
+         $schedule->command('crawl')
+                  ->dailyAt('09:00');
     }
 
     /**
