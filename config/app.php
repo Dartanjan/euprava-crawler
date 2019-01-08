b2a61151e77ec014e8e499e10e81a9602d80abbc
Darko Stanimirovic <darko.stanimirovic@gmail.com>
2019-01-08 10:23:26 +0100

    Add encryption service provider

--- config/app.php
+++ config/app.php
@@ -148,7 +148,7 @@ return [
         Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
         Illuminate\Cookie\CookieServiceProvider::class,
         Illuminate\Database\DatabaseServiceProvider::class,
-        // Illuminate\Encryption\EncryptionServiceProvider::class,
+        Illuminate\Encryption\EncryptionServiceProvider::class,
         Illuminate\Filesystem\FilesystemServiceProvider::class,
         Illuminate\Foundation\Providers\FoundationServiceProvider::class,
         // Illuminate\Hashing\HashServiceProvider::class,
